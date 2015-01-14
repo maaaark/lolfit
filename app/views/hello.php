88249bceac85f3db31a431e122fbf4466ed187f9
Mark Tomicki <marktomicki@gmail.com>
2015-01-14 18:01:41 +0100

    first commit

---
+++ app/views/hello.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<table class="datarow">
+	<tr>
+		<td valign="top" class="champion_avatar">
+			<img src="Aatrox.png"/><br/>
+			<br/>
+			<strong>Current Level:</strong><br/>
+			<br/>
+			<img src="gold_5.png" style="width:85px;">
+		</td>
+		<td valign="top">
+			<table width="100%">
+				<tr class="attr_head">
+					<th>KDA</th>
+					<th>Lasthits</th>
+					<th>Lasthits/m</th>
+					<th>Wards</th>
+					<th>Pinkwards</th>
+					<th>Gold</th>
+					<th>Gold/m</th>
+				</tr>
+				<tr>
+					<td class="mystats">5.2</td>
+					<td class="mystats">180</td>
+					<td class="mystats">6.3</td>
+					<td class="mystats">5</td>
+					<td class="mystats">0.3</td>
+					<td class="mystats">12470</td>
+					<td class="mystats">204</td>
+				</tr>
+				<tr>
+					<td class="average">7.2</td>
+					<td class="average">220</td>
+					<td class="average">9</td>
+					<td class="average">8.3</td>
+					<td class="average">1.2</td>
+					<td class="average">13938</td>
+					<td class="average">340</td>
+				</tr>
+				<tr class="last_game_head">
+					<td colspan="7">Last Game with Aatrox</td>
+				</tr>
+				<tr>
+					<td class="stat_up">+10%</td>
+					<td class="stat_up">+2.3%</td>
+					<td class="stat_down">-2%</td>
+					<td class="stat_up">+6.3%</td>
+					<td class="stat_down">-9%</td>
+					<td class="stat_down">-27%</td>
+					<td class="stat_up">+3%</td>
+				</tr>
+				<tr class="last_game_head">
+					<td colspan="7">Current Skill Levels</td>
+				</tr>
+				<tr>
+					<td class="skill_levels"><img src="gold_5.png" style="width:35px;"></td>
+					<td class="skill_levels"><img src="gold_5.png" style="width:35px;"></td>
+					<td class="skill_levels"><img src="gold_5.png" style="width:35px;"></td>
+					<td class="skill_levels"><img src="gold_5.png" style="width:35px;"></td>
+					<td class="skill_levels"><img src="gold_5.png" style="width:35px;"></td>
+					<td class="skill_levels"><img src="gold_5.png" style="width:35px;"></td>
+					<td class="skill_levels"><img src="gold_5.png" style="width:35px;"></td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
